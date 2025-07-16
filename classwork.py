849e31e880848d08465a3df3a3f97532f5cc266e
radhika-20soni <radhikasoni71007@gmail.com>
2025-07-16 16:52:00 +0530

    Update classwork.py

---
+++ classwork.py
@@ -0,0 +1,37 @@
+#def my_function():
+#print("this is my function")
+#my function()
+
+
+
+#def my_function(fname):
+    #print(fname+"datyal")
+    #my_function("abhay")
+    #my_function("abc")
+   # my_function("asd")
+
+    #def my_function(fname,lname):
+       # print(fname+""+lname)
+        #my_function("Abhay","Datayal")
+
+
+       # def my_function(*students):
+           # print("topper of class is"+students[0])
+            #my_function("Abhay","Aman","Rohit","sahil")
+
+
+            #def my_function(child3,child2,child1):
+                #print("The youngest child is"+child3)
+                #my_function(child1 = "abc," 
+                            #child2="wxy"
+                            #child3="ruv"
+                #)
+#def my_function(**kid):
+    #print("The last name is"+kid["lname"])
+    #my_function(fname = "Abhay",
+                #lname="Datyal")
+numbers = [23,5,42,37,1,30,16,48,55,8,19,51,26,34,44,0,29,53,39,10,31,17,57,3,25,46,22,14,49,6,41,58,20,36,9,52,7,28,45,15,43,2,24,38,11,33,50,4,59,13,18,21,47,27,32,40,54,56]
+length = len(number)
+print(f"This is the length of the list:{length}")
+final_index = (length-1)
+print(f"This is  the final index of the list:{final_index}")
