d34935714274f4176ea14d56ecb319d9e9edc0b6
radhika-20soni <radhikasoni71007@gmail.com>
2025-07-16 16:47:24 +0530

    Update marriage calculator.py

---
+++ marriage calculator.py
@@ -0,0 +1,9 @@
+for i in range (3):
+    age = int (input("enter your age:"))
+    if age>=21:
+        print("You are eligible for marriage.")
+    elif 18<= age <21:
+        print("You are almost eligible.Just wait a bit more!")
+    else:
+        print("Too young for marriage.")
+        
